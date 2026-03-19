d93e6c57d68886a0c5daa6b846845969725cc33f
alex <lescot.alexg@gmail.com>
2026-03-19 15:48:29 +0100

    init backend

---
+++ src/main/java/com/bomberman/backend/HelloResource.java
@@ -0,0 +1,14 @@
+package com.bomberman.backend;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
